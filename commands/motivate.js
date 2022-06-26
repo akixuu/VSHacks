533aa5bca31b5d82d216a19ff790af3255ee5bc5
akixuu <75064265+akixuu@users.noreply.github.com>
2022-06-26 07:29:24 +0000

    the motivational quotes work now ig...?

--- commands/motivate.js
+++ commands/motivate.js
@@ -4,13 +4,16 @@ module.exports = {
     use:'motivate',
     
     execute: async(client, msg, args) => {
+        const fetch = require('node-fetch')
         const response = await fetch('https://zenquotes.io/api/random/');
         const data = await response.json();
         
-        embed = discord.Embed(
-            title = data[0]['q'])
-            .set_footer(
-                text = data[0]['a']);
-        msg.channel.send(embed);
+        const { MessageEmbed } = require('discord.js');
+
+        embed = new MessageEmbed()
+        .setColor('#FFFF00')
+        .setTitle(data[0]['q'])
+        .setFooter(`-${data[0]['a']} | zenquotes.io`);
+        msg.channel.send({ embeds: [embed] });
     }
 }
